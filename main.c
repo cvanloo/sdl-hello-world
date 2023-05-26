a3396b228f5a3970a45704e86177c5589d2a060a
Colin van Loo <colin.vanloo@edu.tbz.ch>
2023-05-26 13:35:56 +0200

    ref: clean-up

--- main.c
+++ main.c
@@ -1,110 +1,119 @@
 /* Compile:
- * gcc -o catamhole main.c `sdl2-config --cflags --libs` -lSDL2_ttf -g
+ * gcc -o catamole main.c -lSDL2 -lSDL2_ttf -g
  */
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdint.h>
 #include <time.h>
-#include <SDL2/SDL.h>
-#include <SDL_ttf.h>
 
-int main(int argc, char** argv) {
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+SDL_Color white = {255, 255, 255, 0};
+TTF_Font *font;
+
+void create_text(SDL_Renderer *renderer, int x, int y, char *text, SDL_Texture **texture, SDL_Rect *rect) {
+    SDL_Surface *message_surface = TTF_RenderText_Solid(font, text, white);
+    *texture = SDL_CreateTextureFromSurface(renderer, message_surface);
+    rect->w = message_surface->w;
+    rect->h = message_surface->h;
+    rect->x = x;
+    rect->y = y;
+    SDL_FreeSurface(message_surface);
+}
+
+int main(int argc, char **argv) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         SDL_Log("SDL initalization failed: `%s'\n", SDL_GetError());
         return EXIT_FAILURE;
     }
 
-    SDL_Window *window = SDL_CreateWindow("Catamohle", SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-    if (window == 0) {
+    SDL_Window *window = SDL_CreateWindow("Catamohle", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH,
+                                          WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
         SDL_Log("SDL creating window failed: `%s'\n", SDL_GetError());
         return EXIT_FAILURE;
     }
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == 0) {
+    if (renderer == NULL) {
         SDL_Log("SDL creating renderer failed: `%s'\n", SDL_GetError());
         return EXIT_FAILURE;
     }
 
-    SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-
-    // ------
-    // Setup font
-    // ------
     TTF_Init();
-    TTF_Font* font = TTF_OpenFont("/usr/share/fonts/truetype/open-sans/OpenSans-Regular.ttf", 24);
-    if (font == 0) {
+    font = TTF_OpenFont("/usr/share/fonts/TTF/DejaVuSans.ttf", 24);
+    if (font == NULL) {
         SDL_Log("SDL failed to open font: `%s'\n", SDL_GetError());
         return EXIT_FAILURE;
     }
-    SDL_Color white = {255, 255, 255};
-    SDL_Surface *message_surface = TTF_RenderText_Solid(font, "Hello, SDL!", white);
-    SDL_Texture *message_texture = SDL_CreateTextureFromSurface(renderer, message_surface);
-    SDL_Rect message_rect;
-    message_rect.x = 10;
-    message_rect.y = 10;
-    message_rect.w = 500;
-    message_rect.h = 100;
-    SDL_RenderCopy(renderer, message_texture, 0, &message_rect);
-    SDL_RenderPresent(renderer);
 
-    SDL_Rect rect;
+    SDL_Rect message_rect;
+    SDL_Rect cat_rect;
+    cat_rect.w = cat_rect.h = 100;
     uint32_t score = 0;
-    struct timespec now, last_rect;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &last_rect);
+    struct timespec now, last_rect_time;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &last_rect_time);
     uint8_t hit = 0;
+
     while (1) {
         SDL_Event event;
         SDL_PollEvent(&event);
         switch (event.type) {
-        case SDL_QUIT: goto exit;
+        case SDL_QUIT:
+            goto exit;
         case SDL_KEYDOWN:
             switch (event.key.keysym.sym) {
-            case SDLK_q: goto exit;
+            case SDLK_q:
+                goto exit;
             }
             break;
         case SDL_MOUSEBUTTONDOWN:
             if (event.button.button == SDL_BUTTON_LEFT) {
                 int x, y;
                 SDL_GetMouseState(&x, &y);
-                if (x >= rect.x && x <= rect.x + 100
-                        && y >= rect.y && y <= rect.y + 100) {
+                if (x >= cat_rect.x && x <= cat_rect.x + 100 && y >= cat_rect.y && y <= cat_rect.y + 100) {
                     ++score;
                     hit = 1;
-                    SDL_Log("hit");
                 }
             }
             break;
         }
 
+        // draw background
         SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
         SDL_RenderClear(renderer);
 
-        char text[100];
-        snprintf(text, 100, "Score: %d", score);
-        SDL_Surface *message_surface = TTF_RenderText_Solid(font, text, white);
-        SDL_Texture *message_texture = SDL_CreateTextureFromSurface(renderer, message_surface);
-        SDL_FreeSurface(message_surface);
+        // draw score
+        char text[10];
+        snprintf(text, 10, "Score: %d", score);
+        SDL_Texture *message_texture;
+        create_text(renderer, 100, 100, text, &message_texture, &message_rect);
         SDL_RenderCopy(renderer, message_texture, 0, &message_rect);
+        SDL_DestroyTexture(message_texture);
 
+        // conditionally update cat position
         clock_gettime(CLOCK_MONOTONIC_RAW, &now);
-        if (hit || now.tv_sec - last_rect.tv_sec > 0.1) {
+        if (hit || now.tv_sec - last_rect_time.tv_sec > 0.1) {
             hit = 0;
-            last_rect = now;
-            rect.x = rand() % (800 - 100);
-            rect.y = rand() % (600 - 100);
-            rect.w = 100;
-            rect.h = 100;
+            last_rect_time = now;
+            cat_rect.x = rand() % (WINDOW_WIDTH - 100);
+            cat_rect.y = rand() % (WINDOW_HEIGHT - 100);
         }
+
+        // draw cat
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderFillRect(renderer, &rect);
+        SDL_RenderFillRect(renderer, &cat_rect);
 
         SDL_RenderPresent(renderer);
     }
 
 exit:
+    TTF_Quit();
+
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
